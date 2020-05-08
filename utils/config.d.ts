8fc37abb77b16bdf802e37900ec186397bade600
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 16:59:43 +0000

    7cc332efe6 docs: update aot compiler docs (#35487)

--- utils/config.d.ts
+++ utils/config.d.ts
@@ -1,12 +1,19 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 /// <amd-module name="@angular/dev-infra-private/utils/config" />
 /**
  * Gets the path of the directory for the repository base.
  */
 export declare function getRepoBaseDir(): string;
 /**
- * Retrieve the configuration from the .dev-infra.json file.
+ * Retrieve the configuration from the .ng-dev-config.js file.
  */
-export declare function getAngularDevConfig<K, T>(): DevInfraConfig<K, T>;
+export declare function getAngularDevConfig<K, T>(supressError?: boolean): DevInfraConfig<K, T>;
 /**
  * Interface exressing the expected structure of the DevInfraConfig.
  * Allows for providing a typing for a part of the config to read.
