71f15d986c9d79f45e5921aa1a2580e2b721830e
atscott <atscott@google.com>
2020-06-09 15:50:27 +0000

    7edb026619 fix(common): prevent duplicate URL change notifications (#37459)

--- utils/shelljs.d.ts
+++ utils/shelljs.d.ts
@@ -7,8 +7,6 @@
  */
 /// <amd-module name="@angular/dev-infra-private/utils/shelljs" />
 import { ExecOptions, ShellString } from 'shelljs';
-/**
- * Runs an given command as child process. By default, child process
- * output will not be printed.
- */
-export declare function exec(cmd: string, opts?: Omit<ExecOptions, 'async'>): ShellString;
+export declare function exec(cmd: string, opts?: ExecOptions & {
+    async?: false;
+}): ShellString;
