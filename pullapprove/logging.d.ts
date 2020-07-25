b5dc4165161f876e40f0bedcc1718eb991fb21d3
Misko Hevery <misko@hevery.com>
2020-07-25 01:20:11 +0000

    67e3ecc7e3 fix(dev-infra): Ensure conditions with groups do not fail verification (#37798)

--- pullapprove/logging.d.ts
+++ pullapprove/logging.d.ts
@@ -7,11 +7,13 @@
  */
 /// <amd-module name="@angular/dev-infra-private/pullapprove/logging" />
 import { PullApproveGroupResult } from './group';
+declare type ConditionGrouping = keyof Pick<PullApproveGroupResult, 'matchedConditions' | 'unmatchedConditions' | 'unverifiableConditions'>;
 /** Create logs for each pullapprove group result. */
-export declare function logGroup(group: PullApproveGroupResult, matched?: boolean, printMessageFn?: {
+export declare function logGroup(group: PullApproveGroupResult, conditionsToPrint: ConditionGrouping, printMessageFn?: {
     (...text: string[]): void;
     group(text: string, collapsed?: boolean | undefined): void;
     groupEnd(): void;
 }): void;
 /** Logs a header within a text drawn box. */
 export declare function logHeader(...params: string[]): void;
+export {};
