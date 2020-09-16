b31b7be4409fdce410826cefd6226ddc144dea72
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:46:08 +0000

    722699fb0c fix(dev-infra): handle no caretaker config being defined (#38862)

--- caretaker/check/github.d.ts
+++ caretaker/check/github.d.ts
@@ -9,4 +9,4 @@
 import { GitClient } from '../../utils/git';
 import { CaretakerConfig } from '../config';
 /** Retrieve the number of matching issues for each github query. */
-export declare function printGithubTasks(git: GitClient, config: CaretakerConfig): Promise<void>;
+export declare function printGithubTasks(git: GitClient, config?: CaretakerConfig): Promise<void>;
