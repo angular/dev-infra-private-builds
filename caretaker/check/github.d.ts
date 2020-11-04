1b5e0b2a8e315298ccedb8ecd99019fa9d8db8ed
Joey Perrott <josephperrott@gmail.com>
2020-11-04 00:54:16 +0000

    2d79780384 fix(dev-infra): run caretaker checks asyncronously (#39086)

--- caretaker/check/github.d.ts
+++ caretaker/check/github.d.ts
@@ -6,7 +6,23 @@
  * found in the LICENSE file at https://angular.io/license
  */
 /// <amd-module name="@angular/dev-infra-private/caretaker/check/github" />
-import { GitClient } from '../../utils/git/index';
-import { CaretakerConfig } from '../config';
-/** Retrieve the number of matching issues for each github query. */
-export declare function printGithubTasks(git: GitClient, config?: CaretakerConfig): Promise<void>;
+import { BaseModule } from './base';
+/** A list of generated results for a github query. */
+declare type GithubQueryResults = {
+    queryName: string;
+    count: number;
+    queryUrl: string;
+    matchedUrls: string[];
+}[];
+export declare class GithubQueriesModule extends BaseModule<GithubQueryResults | void> {
+    retrieveData(): Promise<{
+        queryName: string;
+        count: number;
+        queryUrl: string;
+        matchedUrls: string[];
+    }[] | undefined>;
+    /** Build a Graphql query statement for the provided queries. */
+    private buildGraphqlQuery;
+    printToTerminal(): Promise<void>;
+}
+export {};
