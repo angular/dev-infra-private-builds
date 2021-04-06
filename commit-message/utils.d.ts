c7a171ffda0740ce2da5f4eef29873f2903afed5
atscott <atscott01@gmail.com>
2021-04-06 20:05:32 +0000

    ff9253b4a6 ci: add cache for windows run (#41467)

--- commit-message/utils.d.ts
+++ commit-message/utils.d.ts
@@ -1,6 +1,6 @@
 /// <amd-module name="@angular/dev-infra-private/commit-message/utils" />
-import { Commit } from './parse';
+import { CommitFromGitLog } from './parse';
 /**
  * Find all commits within the given range and return an object describing those.
  */
-export declare function getCommitsInRange(from: string, to?: string): Promise<Commit[]>;
+export declare function getCommitsInRange(from: string, to?: string): Promise<CommitFromGitLog[]>;
