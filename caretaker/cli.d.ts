00046ac49fbc4ed2997693d201a34bbcd9fbd8b9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:56:24 +0000

    74228215a1 build: update dependency @microsoft/api-extractor to v7.18.4 (#42864)

--- caretaker/cli.d.ts
+++ caretaker/cli.d.ts
@@ -8,4 +8,4 @@
 /// <amd-module name="@angular/dev-infra-private/caretaker/cli" />
 import { Argv } from 'yargs';
 /** Build the parser for the caretaker commands. */
-export declare function buildCaretakerParser(yargs: Argv): Argv<import("@angular/dev-infra-private/caretaker/handoff/cli").CaretakerHandoffOptions>;
+export declare function buildCaretakerParser(yargs: Argv): Argv<import("@angular/dev-infra-private/caretaker/check/cli").CaretakerCheckOptions>;
