6f299a50fa20da730ae5f40dbee714b094c9fef4
Joey Perrott <josephperrott@gmail.com>
2021-08-11 15:33:47 +0000

    main - 47cf842 fix(bazel): correct build structure to provide sources of contents of bazel directory (#128)

--- bazel/benchmark/driver-utilities/index.ts
+++ bazel/benchmark/driver-utilities/index.ts
@@ -1,4 +1,3 @@
-/// <amd-module name="dev-infra/bazel/benchmark/driver-utilities/index" />
 /**
  * @license
  * Copyright Google LLC All Rights Reserved.
@@ -6,5 +5,5 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export { openBrowser, verifyNoBrowserErrors } from './e2e_util';
-export { runBenchmark } from './perf_util';
+export {openBrowser, verifyNoBrowserErrors} from './e2e_util';
+export {runBenchmark} from './perf_util';
