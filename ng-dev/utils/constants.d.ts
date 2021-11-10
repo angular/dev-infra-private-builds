2cfe4b98a157927b319a3a00b467ff6233dc3337
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-11-10 22:50:34 +0000

    main - 0474a28 fix(ng-dev/release): properly ensure local ng-dev version is up-to-date

---
+++ ng-dev/utils/constants.d.ts
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+/** NPM package name that is used for the `ng-dev` tool. */
+export declare const ngDevNpmPackageName = "@angular/dev-infra-private";
