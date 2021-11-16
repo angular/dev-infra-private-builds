063c45e3369e114c7156003986cbc9dd657f41d8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-11-16 21:59:36 +0000

    main - 79e9918 refactor: add mandatory extract attributes for api-golden type extraction

--- ng-dev/utils/version-check.mjs
+++ ng-dev/utils/version-check.mjs
@@ -24,7 +24,7 @@ const console_1 = require("./console");
  */
 async function verifyNgDevToolIsUpToDate(workspacePath) {
     // The placeholder will be replaced by the `pkg_npm` substitutions.
-    const localVersion = `0.0.0-912311af713004c92ddc3ac73eb0f43ceb55867f`;
+    const localVersion = `0.0.0-79e9918c9982bc6bac14b3d767d5ae07b7501289`;
     const workspacePackageJsonFile = path.join(workspacePath, constants_1.workspaceRelativePackageJsonPath);
     const workspaceDirLockFile = path.join(workspacePath, constants_1.workspaceRelativeYarnLockFilePath);
     try {
