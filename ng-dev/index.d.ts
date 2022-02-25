09308657601da3ae84d7b1f6d19bf1fe12ec9e46
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-02-25 20:24:49 +0000

    main - ca72f83 build: fix workspace stamping breaking snapshot publishing

---
+++ ng-dev/index.d.ts
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+export * from './utils/config';
+export * from './caretaker/config';
+export * from './commit-message/config';
+export * from './format/config';
+export * from './pr/config';
+export * from './release/config';
+export * from './release/versioning';
+export * from './utils/console';
+export { ReleaseAction } from './release/publish/actions';
+export { FatalReleaseActionError, UserAbortedReleaseActionError, } from './release/publish/actions-error';
