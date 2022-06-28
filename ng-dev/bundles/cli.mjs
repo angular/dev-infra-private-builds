62ea03aa514dc4f039ddddf350ff4621b48d8450
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-06-28 17:40:31 +0000

    main - 29f9ef9 refactor: fix typo in renovate author config

--- ng-dev/bundles/cli.mjs
+++ ng-dev/bundles/cli.mjs
@@ -66170,7 +66170,7 @@ import * as fs3 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a, _b, _c;
-  const localVersion = `0.0.0-474147b76dd2db7876cc05f8e9ef5442651d7102`;
+  const localVersion = `0.0.0-29f9ef912c0c86e9ebbc9d597025171a690658bc`;
   const workspacePackageJsonFile = path2.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path2.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
