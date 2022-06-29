c1d657f35afa50262ad7a06d8e292f9b5ad57ec2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-06-29 17:31:22 +0000

    main - 96d3b40 build: update checked-in update changelog local action

--- ng-dev/bundles/cli.mjs
+++ ng-dev/bundles/cli.mjs
@@ -65971,7 +65971,7 @@ import * as fs3 from "fs";
 import lockfile2 from "@yarnpkg/lockfile";
 async function verifyNgDevToolIsUpToDate(workspacePath) {
   var _a, _b, _c;
-  const localVersion = `0.0.0-eb737068c72a82b168350d76c5b91e709d111cc4`;
+  const localVersion = `0.0.0-96d3b407453ab3baa72425c05e437626f040ae39`;
   const workspacePackageJsonFile = path2.join(workspacePath, workspaceRelativePackageJsonPath);
   const workspaceDirLockFile = path2.join(workspacePath, workspaceRelativeYarnLockFilePath);
   try {
